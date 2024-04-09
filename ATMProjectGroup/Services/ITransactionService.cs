135772d8a2130c25fa3aefb4b9cfc911780336db
ArturRadzivilov <77680872+ArturRadzivilov@users.noreply.github.com>
2024-04-09 21:46:22 +0300

    Add service interface

---
+++ ATMProjectGroup/Services/ITransactionService.cs
@@ -0,0 +1,8 @@
+﻿using ATMProjectGroup.Models;
+
+namespace ATMProjectGroup.Services;
+
+public interface ITransactionService
+{
+	Task<Transaction> TransferMoney(Account sender, Account receiver, decimal amount);
+}
