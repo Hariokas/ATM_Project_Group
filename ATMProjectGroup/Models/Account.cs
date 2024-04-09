5f2cee5a8e04f39557382658d4cdc79cb71872db
ArturRadzivilov <77680872+ArturRadzivilov@users.noreply.github.com>
2024-04-09 20:41:22 +0300

    Create models

---
+++ ATMProjectGroup/Models/Account.cs
@@ -0,0 +1,11 @@
+﻿namespace ATMProjectGroup.Models;
+
+public class Account
+{
+	public Guid Id { get; set; }
+	public string AccountNumber { get; set; } = default!;
+	public decimal Balance { get; set; }
+	public Guid UserId { get; set; }
+	public virtual User User { get; set; }
+	public ICollection<Transaction> Transactions { get; set; } = default!;
+}
