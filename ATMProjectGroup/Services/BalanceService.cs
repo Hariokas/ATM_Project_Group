c87b9e3c36424776f001aa214953fce63199c274
Augustas Dapke <augustas@tikslusmatas.lt>
2024-04-10 19:40:55 +0300

    BalanceService

--- ATMProjectGroup/Services/BalanceService.cs
+++ ATMProjectGroup/Services/BalanceService.cs
@@ -1,17 +1,38 @@
 ﻿using ATMProjectGroup.Models;
+using ATMProjectGroup.Repositories.Interfaces;
 using ATMProjectGroup.Services.Interfaces;
 
 namespace ATMProjectGroup.Services;
 
 public class BalanceService : IBalanceService
 {
-    public Task<decimal> GetBalanceAsync(Account account)
+    private readonly IAccountRepository _accountRepository;
+
+    public BalanceService(IAccountRepository accountRepository)
     {
-        throw new NotImplementedException();
+        _accountRepository = accountRepository;
+    }
+    public async Task<decimal> GetBalanceAsync(Account account)
+    {
+        if (account == null) throw new ArgumentNullException(nameof(account));
+                
+        var retrievedAccount = await _accountRepository.GetAccountByIdAsync(account.Id);
+        if (retrievedAccount == null)
+        {
+            throw new KeyNotFoundException($"No account found with ID {account.Id}");
+        }
+
+        return retrievedAccount.Balance;
+        //throw new NotImplementedException();
     }
 
-    public Task<bool> HasEnoughMoneyAsync(Account account, decimal amount)
+    public async Task<bool> HasEnoughMoneyAsync(Account account, decimal amount)
     {
-        throw new NotImplementedException();
+        if (account == null) throw new ArgumentNullException(nameof(account));
+        if (amount < 0) throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be non-negative.");
+
+        var currentBalance = await GetBalanceAsync(account);
+        return currentBalance >= amount;
+        //throw new NotImplementedException();
     }
 }
