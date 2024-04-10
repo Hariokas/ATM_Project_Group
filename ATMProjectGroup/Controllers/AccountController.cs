bf412c655bb64cc679d7277d9f7b85b14cf4beac
Haris <haroldas.zalg@gmail.com>
2024-04-10 21:00:53 +0300

    Reverted AccountController, implemented UserController

--- ATMProjectGroup/Controllers/AccountController.cs
+++ ATMProjectGroup/Controllers/AccountController.cs
@@ -1,48 +1,42 @@
-﻿using ATMProjectGroup.Models;
-using ATMProjectGroup.Services.Interfaces;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ATMProjectGroup.Controllers
+namespace ATMProjectGroup.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AccountController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class AccountController(IUserService userService) : ControllerBase
+    // GET: api/<AccountController>
+    [HttpGet]
+    public IEnumerable<string> Get()
     {
-        // GET: api/<AccountController>
-        [HttpGet]
-        public async Task<IEnumerable<User>> Get()
-        {
-            return await userService.GetAllUsersAsync();
-        }
+        return new[] { "value1", "value2" };
+    }
 
-        // GET api/<AccountController>/5
-        [HttpGet("{id}")]
-        public async Task<User> Get(Guid id)
-        {
-            return await userService.GetUserByIdAsync(id);
-        }
+    // GET api/<AccountController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
 
-        // POST api/<AccountController>
-        [HttpPost]
-        public void Post([FromBody] User user)
-        {
-            userService.AddUserAsync(user);
-        }
+    // POST api/<AccountController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
 
-        // PUT api/<AccountController>/5
-        [HttpPut("{id}")]
-        public void Put(Guid id, [FromBody] User user)
-        {
-            userService.UpdateUserAsync(user);
-        }
+    // PUT api/<AccountController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
 
-        // DELETE api/<AccountController>/5
-        [HttpDelete("{id}")]
-        public void Delete(Guid id)
-        {
-            userService.DeleteUserAsync(id);
-        }
+    // DELETE api/<AccountController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
     }
 }
--- ATMProjectGroup/Controllers/UserController.cs
+++ ATMProjectGroup/Controllers/UserController.cs
@@ -1,43 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ATMProjectGroup.Models;
+using ATMProjectGroup.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ATMProjectGroup.Controllers
+namespace ATMProjectGroup.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController(IUserService userService) : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserController : ControllerBase
+    // GET: api/<UserController>
+    // GET: api/<AccountController>
+    [HttpGet]
+    public async Task<IEnumerable<User>> Get()
     {
-        // GET: api/<UserController>
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
+        return await userService.GetAllUsersAsync();
+    }
 
-        // GET api/<UserController>/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
+    // GET api/<AccountController>/5
+    [HttpGet("{id}")]
+    public async Task<User> Get(Guid id)
+    {
+        return await userService.GetUserByIdAsync(id);
+    }
 
-        // POST api/<UserController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
+    // POST api/<AccountController>
+    [HttpPost]
+    public void Post([FromBody] User user)
+    {
+        userService.AddUserAsync(user);
+    }
 
-        // PUT api/<UserController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
+    // PUT api/<AccountController>/5
+    [HttpPut("{id}")]
+    public void Put(Guid id, [FromBody] User user)
+    {
+        userService.UpdateUserAsync(user);
+    }
 
-        // DELETE api/<UserController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
+    // DELETE api/<AccountController>/5
+    [HttpDelete("{id}")]
+    public void Delete(Guid id)
+    {
+        userService.DeleteUserAsync(id);
     }
 }
